cfc344caf15104aacbb13d2f612ed2970d7f5f9e
Ilya Verbilo <verbiloilyaa@gmail.com>
2022-11-28 11:25:43 +0300

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,5 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-celery -A config worker --pool=solo -l info
-celery -A config beat -l info
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -4,4 +4,4 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1']
 
 STATIC_URL = '/static/static'
-STAtIC_ROOT = '/vol/web/static/'
+STATIC_ROOT = '/vol/web/static/'
