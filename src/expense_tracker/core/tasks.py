689ff770a4c834c0e7b2757f5551f7d8930e172e
Ilya Verbilo <verbiloilyaa@gmail.com>
2022-11-29 13:27:41 +0300

    typo fixed

--- src/expense_tracker/core/tasks.py
+++ src/expense_tracker/core/tasks.py
@@ -13,13 +13,13 @@ from expense_tracker.core.models import Transaction
 @shared_task
 def send_statistics():
     for user in get_user_model().objects.all():
-        subject = 'EXPENSE ACCOUNTING MANAGER'
-        sum_of_transactions = Transaction.objects.filter(
+        subject = 'EXPENSE TRACKER'
+        transaction_amount_per_24h = Transaction.objects.filter(
             user=user,
             date__range=[
                 datetime.date.today(),
                 datetime.date.today() - datetime.timedelta(days=1)]
         ).aggregate(Sum('amount'))
-        message = f'Сумма транзакций за последние 24 часа: {sum_of_transactions.get("ammount__sum", 0)}'
+        message = f'Сумма транзакций за последние 24 часа: {transaction_amount_per_24h.get("amount__sum", 0)}'
         send_mail(subject=subject, message=message, from_email=settings.EMAIL_HOST_USER, recipient_list=[user.email],
                   fail_silently=False)
